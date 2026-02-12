39f2f62bcfde1256cf637d849eda6df3262be74a
luis05t <148250236+luis05t@users.noreply.github.com>
2026-02-12 17:48:33 -0500

    recuperar

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ import RegisterPage from './pages/RegisterPage'
 import Dashboad from './pages/Dashboard'
 import ProyectPage from './pages/ProyectPage'
 
+// 1. Asegúrate de que los nombres de importación coincidan con el uso abajo
+import ForgotPassword from './pages/ForgotPassword' 
+import ResetPassword from './pages/ResetPassword'
+
 function App() {
   return (
     <Routes>
@@ -14,9 +18,16 @@ function App() {
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/dashboard" element={<Dashboad  />} />
       
-      {/* RUTA DINÁMICA: IMPORTANTE */}
+      {/* 2. Usa los nombres exactos que importaste arriba */}
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/reset-password/:token" element={<ResetPassword />} />
+
+      {/* RUTA DINÁMICA */}
       <Route path="/projects" element={<ProyectPage  />} />
       <Route path="/projects/:id" element={<ProyectPage  />} />
+
+      {/* RUTA PARA MANEJAR ERRORES 404 */}
+      <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   )
 }
